4b1d70f1915896f5f196c2e5c2c48ebf0ccede89
Charles Rethman <rethman@me.com>
2016-08-05 18:41:15 +0200

    Updated data_structures.js

--- data_structures.js
+++ data_structures.js
@@ -1,46 +1,57 @@
-'use strict';
+(function() {
+   'use strict';
 
-function Stack() {
-   var arr = [];
+   /**
+    * Private Stack prototype constructor function. Creates a private array to hold all the stack
+    * values that is not visible to the outside world, except for the top (array last element) and
+    * bottom (array first element) values.
+    *
+    * @param value {object | array | string | number}. The value to be pushed onto the stack. Can be
+    * any data type (even another stack!).
+    *
+    * @return {object}. Contains the push, pop and clean methods as well as the stack top, bottom
+    * and size values.
+    *
+    */
+   function Stack() {
+      // private array to hold all the stack items.
+      var arr = [];
+      // private function to update the Stack object property values withnthe current array values
+      function updateArrayValues(obj) {
+         obj.size = arr.length;
+         obj.top = arr[arr.length - 1];
+         obj.bottom = arr[0];
+      }
+      // returns composite object
+      return {
+         // Push method, takes value and pushes it onto the end of the stack array, passing the stack to the function have its properties updated. @param value {Object | array | string | number}. See function comment above. @return {number}, The new size of he stack.
+         push : function(value) {
+            arr.push(value);
+            updateArrayValues(this);
+            return this.size;
+         },
+         // Pop method, takes top value off the stack, popping it off the end of the stack array, pasing the stack object to the function to have its properties updated. @param none. @return {object | array | string | number} which was previously on top of the stack.
+         pop : function() {
+            var top = arr.pop();
+            updateArrayValues(this);
+            return top;
+         },
+         // Clear method, clears out the stack laving it empty and the top and bottom undefined and the size 0. Passes the stack object to the function to update its properties. @param none. @return true {boolean} to indicate success.
+         clear : function() {
+            arr = [];
+            updateArrayValues(this);
+            return true
+         },
+         // Size property, the number of items in the stack (supplied by the array length).
+         size : arr.length,
 
-   function updateArrayValues(obj) {
-      obj.size = arr.length;
-      obj.top = arr[arr.length - 1];
-//      obj.next = arr[arr.length - 2];
-      obj.bottom = arr[0];
-   }
+         top : arr[arr.length - 1],
 
-   return {
+         bottom : arr[0]
 
-      push : function(value) {
-         arr.push(value);
-         updateArrayValues(this);
-         return this.size;
-      },
-
-      pop : function(value) {
-         var top = arr.pop();
-         updateArrayValues(this);
-         return top;
-      },
-
-      clear : function() {
-         arr = [];
-         updateArrayValues(this);
-         return true
-      },
-
-      size : arr.length,
-
-      top : arr[arr.length - 1],
-
-//      next : arr[arr.length - 2],
-
-      bottom : arr[0]
+      };
 
    };
-
-};
 
 function Queue() {
    var arr = [];
@@ -110,59 +121,59 @@ function Queue() {
    }
 };*/
 
-
-function LinkedList() {}
-LinkedList.prototype = {
-  length: 0,
-  first: null,
-  last: null
+/**
+ * Creates a LinkedList from an array.
+ */
+function LinkedList(arr) {
+   console.log(arr);
+   var last = null;
+   for (var i = 0; i < arr.length; i++) {
+      var node = {
+         value: arr[i]
+      };
+      console.log(node);
+      if (!last) {
+         this.head = node;
+      } else {
+         last.next = node;
+      }
+      last = node;
+   }
+}
+/**
+ * LinkedList iterator
+ */
+LinkedList.prototype.iterator = function() {
+   var list = this;
+   return {
+      next: function() {
+         if (this.node && !this.node.next) {
+            throw new Error('At tail of list!');
+         }
+         this.node = this.node ? this.node.next : list.head;
+         return this.node.value;
+      },
+      hasNext: function() {
+         if (!this.node) return list.head;
+         return this.node.next;
+      }
+   };
+};
+/**
+ * Converts this LinkedList to an array.
+ */
+LinkedList.prototype.toArray = function() {
+   var it = this.iterator();
+   var ret = [];
+   while (it.hasNext()) {
+      ret.push(it.next());
+   }
+   return ret;
 };
 
-LinkedList.Circular = function() {};
-LinkedList.Circular.prototype = new LinkedList();
 
-LinkedList.Circular.prototype.append = function(node) {
-  if (this.first === null) {
-    node.prev = node;
-    node.next = node;
-    this.first = node;
-    this.last = node;
-  } else {
-    node.prev = this.last;
-    node.next = this.first;
-    this.first.prev = node;
-    this.last.next = node;
-    this.last = node;
-  }
-  this.length++;
-};
+exports.Stack = Stack;
+exports.Queue = Queue;
+exports.LinkedList = LinkedList;
 
-LinkedList.Circular.prototype.insertAfter = function(node, newNode) {
-  newNode.prev = node;
-  newNode.next = node.next;
-  node.next.prev = newNode;
-  node.next = newNode;
-  if (newNode.prev == this.last) { this.last = newNode; }
-  this.length++;
-};
-
-LinkedList.Circular.prototype.remove = function(node) {
-  if (this.length > 1) {
-    node.prev.next = node.next;
-    node.next.prev = node.prev;
-    if (node == this.first) { this.first = node.next; }
-    if (node == this.last) { this.last = node.prev; }
-  } else {
-    this.first = null;
-    this.last = null;
-  }
-  node.prev = null;
-  node.next = null;
-  this.length--;
-};
-
-
-exports.stack = new Stack;
-exports.queue = new Queue;
-exports.linkedList = new LinkedList;
-exports.linkedListCircular = new LinkedList.Circular;
+}());
